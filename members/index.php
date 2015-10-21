80a417d203a61f22b2f155c47e3e2f5b2534e3de
Sarah German <sarah@advomatic.com>
2015-10-21 13:17:59 -0500

    Style members page.

--- members/index.php
+++ members/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Northeast Brewers + Distillers Association</title>
+    <title>Northeast Brewers + Distillers Association | Members</title>
     <link rel="stylesheet" href="/css/foundation.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Oswald:400,700|Quattrocento+Sans:400,700' rel='stylesheet' type='text/css'>
@@ -24,19 +24,8 @@
 
     <div class="row">
       <div class="large-8 medium-8 columns">
-        <div class="title"><h1>Events</h1></div>
-        <h2>The Northeast Halloween Crawl</h2>
-        <p><img src="/img/events/halloween.jpg"/></p>
-
-        <p>Halloween: what’s better than going door to door in search for delicious candy? Going door to door in search for delicious adult beverages! To celebrate what is truly the adult version of Halloween candy; the good people at <a href="http://bitterminnesotabrewerytours.com/">Bitter Minnesota</a> and the Northeast Brewers & Distillers Association (NEBDA) are hosting a Northeast Halloween Taproom Crawl on <strong>Saturday, Oct. 31 starting at 7 p.m.</strong></p>
-
-        <p>Compliments of Bitter Minnesota Brewery Tours, the evening will feature free shuttles to and from some of the best tap and cocktail rooms throughout Northeast Minneapolis.</p>
-
-        <p>Each tap and cocktail room is pulling out all the stops in a different way, guaranteeing an amazing and unique experience at each locale. The evening will feature everything from scary movies to costume contests to dance parties to haunted breweries and more.</p>
-
-        <p>While each of NEBDA members is creating its own adventure for the night, the overall goal is to bring the brewing and distilling communities together and give drinkers yet another excuse to celebrate in Northeast!</p>
-
-        <p><a href="https://www.facebook.com/events/468207233362819/" class="secondary small button">View Facebook event page →</a></p>
+        <div class="title"><h1>Our Members</h1></div>
+        <div class="member-logos"><?php include_once('../inc/functions.php'); echo nebda_get_member_logos(); ?></div>
       </div>
 
       <div class="large-4 medium-4 columns">
@@ -69,5 +58,6 @@
         </div>
       </div>
     </footer>
+
   </body>
 </html>

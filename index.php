869d86135e12dee1b9d8a06d49992bbebd68fb8b
Sarah German <sarah@advomatic.com>
2015-10-20 22:02:51 -0500

    Use PHP file extension, add content.

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Northeast Brewers + Distillers Association</title>
+    <link rel="stylesheet" href="css/foundation.min.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700|Quattrocento+Sans:400,700' rel='stylesheet' type='text/css'>
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+    <div class="row">
+      <div class="large-8 columns">
+        <a href="/" class="logo"><img src="/img/nebda.png" alt="NEBDA logo"></a>
+      </div>
+      <div class="large-4 columns">
+        <ul class="menu-bar">
+          <li><a href="/members">Our Members</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="large-8 medium-8 columns">
+        <div class="title"><h1>Events</h1></div>
+        <h2>The Northeast Halloween Crawl</h2>
+        <p><img src="/img/events/halloween.jpg"/></p>
+
+        <p>Halloween: what’s better than going door to door in search for delicious candy? Going door to door in search for delicious adult beverages! To celebrate what is truly the adult version of Halloween candy; the good people at <a href="http://bitterminnesotabrewerytours.com/">Bitter Minnesota</a> and the Northeast Brewers & Distillers Association (NEBDA) are hosting a Northeast Halloween Taproom Crawl on <strong>Saturday, Oct. 31 starting at 7 p.m.</strong></p>
+
+        <p>Compliments of Bitter Minnesota Brewery Tours, the evening will feature free shuttles to and from some of the best tap and cocktail rooms throughout Northeast Minneapolis.</p>
+
+        <p>Each tap and cocktail room is pulling out all the stops in a different way, guaranteeing an amazing and unique experience at each locale. The evening will feature everything from scary movies to costume contests to dance parties to haunted breweries and more.</p>
+
+        <p>While each of NEBDA members is creating its own adventure for the night, the overall goal is to bring the brewing and distilling communities together and give drinkers yet another excuse to celebrate in Northeast!</p>
+
+        <p><a href="https://www.facebook.com/events/468207233362819/" class="secondary small button">View Facebook event page →</a></p>
+      </div>
+
+      <div class="large-4 medium-4 columns">
+        <div class="social-icons">
+        </div>
+        <div class="panel">
+          <h3>About NEBDA</h3>
+          <p>The mission of this organization is to improve our community and strengthen our industry by providing a platform for communication and collaboration for all breweries, distilleries, and cideries located in Northeast Minneapolis.</p>
+        </div>
+      </div>
+    </div>
+
+    <footer class="row">
+      <div class="large-12 columns">
+        <hr/>
+        <div class="row">
+          <div class="large-6 columns">
+            <p>&copy; <?php echo date('Y') ?> Northeast Brewers + Distillers Association </p>
+          </div>
+          <div class="large-6 columns">
+            <ul class="inline-list right">
+              <li><a href="/members">Our Members</a></li>
+              <li><a href="/contact">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>

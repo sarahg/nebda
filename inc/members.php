b07346f6125907b109010a9706ebf6a869529505
Sarah German <sarah@advomatic.com>
2015-10-21 13:22:49 -0500

    Add links to breweries

--- inc/members.php
+++ inc/members.php
@@ -1,71 +1,69 @@
 <?php
 
-// @todo fill in websites
-
 $members = array(
   array(
     'name' => '56 Brewing',
     'image' => '56.png',
-    'url' => 'http://'
+    'url' => 'http://56brewing.com/'
   ),
   array(
     'name' => '612 Brew',
     'image' => '612.png',
-    'url' => 'http://'
+    'url' => 'http://www.612brew.com/'
   ),
   array(
     'name' => 'Bauhaus Brew Labs',
     'image' => 'bauhaus.png',
-    'url' => 'http://'
+    'url' => 'http://bauhausbrewlabs.com/'
   ),
   array(
     'name' => 'Boom Island',
     'image' => 'boom_island.png',
-    'url' => 'http://'
+    'url' => 'http://boomislandbrewing.com/'
   ),
   array(
     'name' => 'Dangerous Man',
     'image' => 'dangerous_man.png',
-    'url' => 'http://'
+    'url' => 'http://dangerousmanbrewing.com/'
   ),
   array(
     'name' => 'Fair State',
     'image' => 'fair_state.png',
-    'url' => 'http://'
+    'url' => 'http://fairstate.coop/'
   ),
   array(
     'name' => 'Indeed Brewing',
     'image' => 'indeed.png',
-    'url' => 'http://'
+    'url' => 'http://www.indeedbrewing.com/'
   ),
   array(
     'name' => 'Insight Brewing',
     'image' => 'insight.png',
-    'url' => 'http://'
+    'url' => 'http://www.insightbrewing.com/'
   ),
   array(
     'name' => 'Norseman Distillery',
     'image' => 'norseman.png',
-    'url' => 'http://'
+    'url' => 'http://www.norsemandistillery.com/'
   ),
   array(
     'name' => 'NorthGate Brewing',
     'image' => 'northgate.png',
-    'url' => 'http://'
+    'url' => 'http://www.northgatebrew.com/'
   ),
   array(
     'name' => 'Tattersall Distillery',
     'image' => 'tattersall.png',
-    'url' => 'http://'
+    'url' => 'http://www.tattersalldistilling.com/'
   ),
   array(
     'name' => 'Sociable Cider Werks',
     'image' => 'sociable.png',
-    'url' => 'http://'
+    'url' => 'http://sociablecider.com/'
   ),
   array(
     'name' => 'Wander North Distillery',
     'image' => 'wander.png',
-    'url' => 'http://'
+    'url' => 'http://wandernorthdistillery.com/'
   )
 );
